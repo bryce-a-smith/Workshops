a1268ac1666087ff491bbb06a4dccd1a0763d6af
BRYCE SMITH <a.bryce.smith.gmail.com>
2023-12-15 12:40:06 -0500

    cards

--- StayOrganized/scripts/todos.js
+++ StayOrganized/scripts/todos.js
@@ -1,36 +1,75 @@
-"use strict"
+"use strict";
 
 function init() {
-    //query selector function
-    // function $(selector) {
-    //     return document.querySelector(selector);
-    // }
-    let $ = (selector) => document.querySelector(selector);
+  //query selector function
+  // function $(selector) {
+  //     return document.querySelector(selector);
+  // }
+  let $ = (selector) => document.querySelector(selector);
 
-    //Get HTML elements
-    // const usersSelect = document.querySelector("#users-select");
-    const usersSelect = $("#users-select");
+  //Get HTML elements
+  // const usersSelect = document.querySelector("#users-select");
+  const usersSelect = $("#users-select");
 
-    //Functions
-    async function loadUsersSelect() {
-        fetch("http://localhost:8083/api/users")
-        .then((response) => response.json())
-        .then((data) => {
-            for(let user of data) {
-                userOptionSelect(user);
-            }
-        });
+  //Functions
+  async function loadUsersSelect() {
+    fetch("http://localhost:8083/api/users")
+      .then((response) => response.json())
+      .then((data) => {
+        for (let user of data) {
+          userOptionSelect(user);
+        }
+      });
+  }
 
-    }
+  async function userOptionSelect(user) {
+    let userOption = new Option(user.username, user.id);
 
-    async function userOptionSelect(user) {
-        let userOption = new Option(user.username, user.id);
+    usersSelect.appendChild(userOption);
+  }
 
-        usersSelect.appendChild(userOption);
-    }
+  async function displayTodos() {
+    fetch(`http://localhost:8083/api/todos/byuser/${usersSelect.value}`)
+      .then((response) => response.json())
+      .then((data) => {
+        for (let todo of data) {
+            console.log(todo);
+          userOptionSelect(todo);
+        }
+      });
+  }
 
-    //Function calls and wire-ups
-    loadUsersSelect()
+//   async function buildAndDisplayCard(todo) {
+//     let currentCard = document.createElement("div");
+
+//     let currentH3 = document.createElement("h3");
+//     let currentH5 = document.createElement("h5");
+//     let currentP = document.createElement("p");
+//     let currentP2 = document.createElement("p");
+
+//     currentCard.classList.add("card");
+
+
+//     currentH3.innerText = park.LocationName;
+//     currentH5.innerText = `${park.Address}\n${park.City}, ${park.State} ${park.ZipCode}`;
+//     currentP.innerText = `Phone: ${park.Phone}\nFax: ${park.Fax}`;
+//     currentP2.innerText = `(${park.Latitude},${park.Longitude})`;
+
+
+
+//     currentCard.appendChild(currentH3);
+//     currentCard.appendChild(currentH5);
+//     currentCard.appendChild(currentP);
+//     currentCard.appendChild(currentP2);
+
+
+//     displayParksDiv.appendChild(currentCard);
+//   }
+
+  //Function calls and wire-ups
+  loadUsersSelect();
+
+  usersSelect.addEventListener("change", displayTodos);
 }
 
 window.onload = init;
